f20a2a955fcb61c2010cd96d95896932706404a7
mleuatd <mleuatd@gmail.com>
2026-09-16 18:47:42 +0900

    chore: refresh character module cache after limb review

--- site/app.js
+++ site/app.js
@@ -1,6 +1,6 @@
 import {makeSample,parseChart} from "./parsers.js";
 import {decodeAudio,analyzeOnsets,realignNotes} from "./audio-analysis.js";
-import {initCharacterPrototype,updateCharacterPrototype} from "./character-prototype.js?v=20260916-full-song-qa";
+import {initCharacterPrototype,updateCharacterPrototype} from "./character-prototype.js?v=20260916-limb-review";
 
 const PARTS=["LB","LC","HH","LP","SN","BD","HT","LT","FT","RD","RC"];
 const PART_LABEL={LC:"左シンバル",HH:"ハイハット",SN:"スネア",HT:"ハイタム",LT:"ロータム",FT:"フロアタム",RC:"右シンバル",RD:"ライド",LP:"左足HH",LB:"左足BD",BD:"バスドラム"};
