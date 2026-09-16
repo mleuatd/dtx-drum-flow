1a30043961d0f19706c077e448f6e46c5b66e9c8
mleuatd <mleuatd@gmail.com>
2026-09-17 06:50:29 +0900

    site: refresh Luna neutral and pose sprites

--- site/character-prototype.js
+++ site/character-prototype.js
@@ -4,7 +4,7 @@ const PROTOTYPE_MEASURE_END=8;
 const LIMB_URL="./charts/luna_say_maybe/Luna_say_maybe_1_16_limbs.json";
 const INVENTORY_URL="./character-assets/prototypes/luna_say_maybe_16m/asset_inventory.json";
 const ASSET_ROOT="./character-assets";
-const ASSET_VERSION="20260917-android-character-visible-r4";
+const ASSET_VERSION="20260917-neutral-and-pose-r5";
 const DRUM="./character-assets/layers/drum/drum_base.png";
 const assetUrl=src=>src+(src.includes("?")?"&":"?")+"v="+ASSET_VERSION;
 
