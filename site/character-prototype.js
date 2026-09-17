e455f57c5ed4caf1fb5921303e51915bf9ee8146
mleuatd <mleuatd@gmail.com>
2026-09-18 07:05:54 +0900

    site: bump neutral asset cache version

--- site/character-prototype.js
+++ site/character-prototype.js
@@ -4,7 +4,7 @@ const PROTOTYPE_MEASURE_END=4;
 const LIMB_URL="./charts/luna_say_maybe/Luna_say_maybe_1_16_limbs.json";
 const INVENTORY_URL="./character-assets/prototypes/luna_say_maybe_16m/asset_inventory.json";
 const ASSET_ROOT="./character-assets";
-const ASSET_VERSION="20260918-drum-compact-v2-r1";
+const ASSET_VERSION="20260918-neutral-home-644ccf0a-r1";
 const DRUM="./character-assets/layers/drum/drum_base.png";
 const assetUrl=src=>src+(src.includes("?")?"&":"?")+"v="+ASSET_VERSION;
 
