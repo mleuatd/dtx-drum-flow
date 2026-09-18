874c759783d4f57efea99f6b55103b4715cbecbf
mleuatd <mleuatd@gmail.com>
2026-09-18 11:25:43 +0900

    site: refresh M1-4 final 10 asset cache

--- site/character-prototype.js
+++ site/character-prototype.js
@@ -4,7 +4,7 @@ const PROTOTYPE_MEASURE_END=4;
 const LIMB_URL="./charts/luna_say_maybe/Luna_say_maybe_1_16_limbs.json";
 const INVENTORY_URL="./character-assets/prototypes/luna_say_maybe_16m/asset_inventory.json";
 const ASSET_ROOT="./character-assets";
-const ASSET_VERSION="20260918-m1-4-posefix-final-r2";
+const ASSET_VERSION="20260918-m1-4-final10-r3";
 const DRUM="./character-assets/layers/drum/drum_base.png";
 const assetUrl=src=>src+(src.includes("?")?"&":"?")+"v="+ASSET_VERSION;
 
